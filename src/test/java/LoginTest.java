87c6def86eb869b27b598740f3be82259234c711
hakuban <hakuban@yahoo.com>
2019-08-06 20:26:21 -0400

    initial commit

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,5 +1,6 @@
 public class LoginTest {
     public static void main(String[] args) {
-        System.out.println("login");
+        System.out.println("line 1");
+        System.out.println("line 2");
     }
 }
